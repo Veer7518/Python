147e20b382488ded06166e8f2e9470f0eb858b00
Veer Singh <153379985+Veer7518@users.noreply.github.com>
2024-08-04 23:27:04 +0530

    Add files via upload

---
+++ Condition.py
@@ -0,0 +1,22 @@
+"""myage =25
+if(myage>=18):
+    print("you are take the vote")"""
+#elif
+"""myage =19
+if(myage>=20):
+    print("you are take the vote because your age 18")
+elif(myage>=18):
+  print("you are take the vote because your age 18")"""
+  #else
+myage = 17
+if(myage>=20):
+    print("you are take the vote because your age 20")
+elif(myage>=18):
+  print("you are take the vote because your age 18")
+else:
+   print("your are not take the votes because you age lese then 20 and 18 ")
+
+
+
+
+    
